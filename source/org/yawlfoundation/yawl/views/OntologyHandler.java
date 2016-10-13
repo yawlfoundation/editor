ab64498faf61f107a67087e599c9cd4d77f6a37d
Michael Adams <mj.adams@qut.edu.au>
2016-10-13 20:25:33 +0200

    bugfix: exlet editor validation false negative (post-item constraint, remove case)

--- source/org/yawlfoundation/yawl/views/OntologyHandler.java
+++ source/org/yawlfoundation/yawl/views/OntologyHandler.java
@@ -1,6 +1,7 @@
 package org.yawlfoundation.yawl.views;
 
 import org.apache.jena.ontology.OntModel;
+import org.apache.jena.query.*;
 import org.yawlfoundation.yawl.editor.core.YSpecificationHandler;
 import org.yawlfoundation.yawl.views.ontology.OntologyPopulator;
 import org.yawlfoundation.yawl.views.ontology.SpecificationParser;
@@ -33,4 +34,26 @@ public class OntologyHandler {
         _ontModel = ontologyPopulator.getOntModel();
     }
 
+
+    public static ResultSet query(String query) {
+        Query sparql = QueryFactory.create(query, Syntax.syntaxARQ);
+        QueryExecution qExec = QueryExecutionFactory.create(sparql, _ontModel.getBaseModel());
+        return qExec.execSelect();
+    }
+
+//    String query = "SELECT ?subject ?predicate ?object \n" +
+//                       "WHERE { \n" +
+//                       "?subject ?predicate ?object }";
+
+
+//    while(rs.hasNext())
+//        {
+//            QuerySolution sol = rs.nextSolution();
+//            RDFNode object = sol.get("object");
+//            RDFNode predicate = sol.get("predicate");
+//            RDFNode subject = sol.get("subject");
+//
+//            DefaultTableModel model = (DefaultTableModel) table.getModel();
+//            model.addRow(new Object[]{subject, predicate, object});
+//        }
 }
